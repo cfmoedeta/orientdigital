62d602f85bdb433ea21e9ece0996b455b118c71c
Ahmad AbdEl-Aal <ahmad.abdelaallabas@gmail.com>
2025-05-27 16:23:16 +0300

    style: add sticky to navbar to make it stick at top when scrolling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,15 @@ import Logo from "./Logo";
 
 export default function Navbar() {
   return (
-    <nav className="fit-body">
-      <Logo />
-      <div className="links">
-        <Link href={"/#about"}>About</Link>
-        <Link href={"/#talents"}>Talents</Link>
-        <Link href={"/#services"}>Services</Link>
-        <Link href={"/#contact"}>Contact</Link>
+    <nav>
+      <div className="navbar-content fit-body">
+        <Logo />
+        <div className="links">
+          <Link href={"/#about"}>About</Link>
+          <Link href={"/#talents"}>Talents</Link>
+          <Link href={"/#services"}>Services</Link>
+          <Link href={"/#contact"}>Contact</Link>
+        </div>
       </div>
     </nav>
   );

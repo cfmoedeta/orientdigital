704f65d533d9f494e343c01ca879c505a7c5da40
Ahmad AbdEl-Aal <ahmad.abdelaallabas@gmail.com>
2025-05-30 15:12:24 +0300

    feat: add moving neon borders to service cards

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,9 +1,11 @@
 export default function ServiceCard({ service }) {
   return (
     <div className="service-card">
-      <img src={service.image} alt={service.title} />
-      <h3>{service.title}</h3>
-      <p>{service.description}</p>
+      <div className="service-data">
+        <img src={service.image} alt={service.title} />
+        <h3>{service.title}</h3>
+        <p>{service.description}</p>
+      </div>
     </div>
   );
 }

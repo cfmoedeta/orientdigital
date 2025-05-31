213443fa8d74cfde507c7a4ffc2b054179b8ddda
Ahmad AbdEl-Aal <ahmad.abdelaallabas@gmail.com>
2025-05-31 16:42:36 +0300

    feat: create separate page for all talents, add more talents

---
+++ src/app/talents/page.js
@@ -0,0 +1,33 @@
+import "@/css/ManagedTalents.css";
+import { talents } from "@/components/ManagedTalents";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export default function page() {
+  return (
+    <>
+      <Navbar />
+      <main className="talents fit-body" style={{ marginTop: "0" }}>
+        <h1 className="main-heading">
+          Our Managed <span style={{ color: "#ff6b35" }}>Talents</span>
+        </h1>
+        <div className="talents-container">
+          {talents.map((talent) => (
+            <div key={talent.name} className="talent-card">
+              <div className="card-inner">
+                <div className="card-front">
+                  <img src={talent.image} alt={talent.name} loading="lazy" />
+                </div>
+                <div className="card-back">
+                  <h1>{talent.name}</h1>
+                  <h3 style={{ color: "#34d399" }}>{talent.talent}</h3>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+}
--- src/components/ManagedTalents.js
+++ src/components/ManagedTalents.js
@@ -1,28 +1,80 @@
 import "@/css/ManagedTalents.css";
 import HeaderWrapper from "./wrappers/header-wrapper";
+import Link from "next/link";
 
-const talents = [
+export const talents = [
+  {
+    name: "Tamer Hosny",
+    image: "/talents/tamer-hosny.jpg",
+    talent: "Musician",
+  },
+  {
+    name: "Mahmoud El-Esseily",
+    image: "/talents/esseily.jpg",
+    talent: "Musician",
+  },
+  { name: "Mohamed Hamaki", image: "/talents/hamaki.jpeg", talent: "Musician" },
+  { name: "Angham", image: "/talents/angham.jpg", talent: "Musician" },
+  {
+    name: "Bahaa Sultan",
+    image: "/talents/bahaa-sultan.jpg",
+    talent: "Musician",
+  },
+  {
+    name: "Ehab Tawfik",
+    image: "/talents/ehab-tawfik.jpg",
+    talent: "Musician",
+  },
+  {
+    name: "Hisham Abbas",
+    image: "/talents/hisham-abbas.jpg",
+    talent: "Musician",
+  },
+  {
+    name: "Carolina Karam",
+    image: "/talents/carolina-karam.jpg",
+    talent: "Musician",
+  },
+  {
+    name: "Mahmoud Hegazy",
+    image: "/talents/mahmoud-hegazy.jpg",
+    talent: "Actor",
+  },
+  { name: "Sherine", image: "/talents/sherine.jpeg", talent: "Musician" },
+  {
+    name: "Ragheb Alama",
+    image: "/talents/ragheb-alama.jpg",
+    talent: "Musician",
+  },
+  {
+    name: "Reda El-Bahrawy",
+    image: "/talents/reda-elbahrawy.jpg",
+    talent: "Musician",
+  },
   { name: "Wegz", image: "/talents/wegz.jpg", talent: "Musician" },
   {
     name: "Yasmina El-Abd",
     image: "/talents/yasmina-elabd.jpg",
     talent: "Actress",
   },
-  {
-    name: "Tasneem El-Aidy",
-    image: "/talents/tasneem-elaidy.jpg",
-    talent: "Influencer",
-  },
   { name: "Taha Desouky", image: "/talents/taha-desouky.jpg", talent: "Actor" },
+  { name: "Ahmed Dash", image: "/talents/ahmed-dash.jpg", talent: "Actor" },
+  {
+    name: "Huda El-Mufti",
+    image: "/talents/huda-elmufti.jpg",
+    talent: "Actress",
+  },
   {
     name: "Sarah Hany",
     image: "/talents/sarah-hany.jpg",
     talent: "Influencer",
   },
+  { name: "Flex", image: "/talents/flex.jpg", talent: "Musician" },
+  { name: "Husayn", image: "/talents/husayn.jpg", talent: "Musician" },
   {
-    name: "Raouf El-Sherif",
-    image: "/talents/raouf-elsherif.jpg",
-    talent: "Influencer",
+    name: "Eyad EL-Mogy",
+    image: "/talents/eyad-elmogy.jpg",
+    talent: "YouTuber",
   },
   {
     name: "Mohammed Tarek",
@@ -34,30 +86,22 @@ const talents = [
     image: "/talents/karim-dash.jpeg",
     talent: "Influencer",
   },
-  { name: "Husayn", image: "/talents/husayn.jpg", talent: "Musician" },
+  {
+    name: "Tasneem El-Aidy",
+    image: "/talents/tasneem-elaidy.jpg",
+    talent: "Influencer",
+  },
   {
     name: "Hassan Gabr",
     image: "/talents/hassan-gabr.jpg",
     talent: "Influencer",
   },
-  {
-    name: "Huda El-Mufti",
-    image: "/talents/huda-elmufti.jpg",
-    talent: "Actress",
-  },
-  { name: "Flex", image: "/talents/flex.jpg", talent: "Musician" },
-  {
-    name: "Eyad EL-Mogy",
-    image: "/talents/eyad-elmogy.jpg",
-    talent: "YouTuber",
-  },
   { name: "Dina Dash", image: "/talents/dina-dash.jpg", talent: "Influencer" },
   {
-    name: "Ahmed El-Gendy",
-    image: "/talents/ahmed-elgendy.jpeg",
-    talent: "Athlete",
+    name: "Raouf El-Sherif",
+    image: "/talents/raouf-elsherif.jpg",
+    talent: "Influencer",
   },
-  { name: "Ahmed Dash", image: "/talents/ahmed-dash.jpg", talent: "Actor" },
 ];
 
 export default function ManagedTalents() {
@@ -67,7 +111,7 @@ export default function ManagedTalents() {
         Some of the <span style={{ color: "#ff6b35" }}>Talents</span> We Manage
       </HeaderWrapper>
       <div className="talents-container">
-        {talents.map((talent) => (
+        {talents.slice(0, 16).map((talent) => (
           <div key={talent.name} className="talent-card">
             <div className="card-inner">
               <div className="card-front">
@@ -81,6 +125,9 @@ export default function ManagedTalents() {
           </div>
         ))}
       </div>
+      <Link href={"/talents"} className="browse-talents-button">
+        Browse All Talents
+      </Link>
     </section>
   );
 }

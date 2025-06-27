5e7b35fbf694ebdcf69ea3bd0ce54445d347664c
MoeDeta <108597141+cfmoedeta@users.noreply.github.com>
2025-06-27 14:48:19 +0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "@/css/global.css";
+import Script from 'next/script';
 
 export const metadata = {
   title: "Orient Digital | Talent Management",
@@ -32,6 +33,32 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+<head>
+  <Script id="facebook-pixel" strategy="afterInteractive">
+    {`
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1051013736095607'); 
+      fbq('track', 'PageView');
+    `}
+  </Script>
+
+  <noscript>
+    <img
+      height="1"
+      width="1"
+      style={{ display: "none" }}
+      src="https://www.facebook.com/tr?id=1051013736095607&ev=PageView&noscript=1"
+    />
+  </noscript>
+</head>
+
       <body>{children}</body>
     </html>
   );
